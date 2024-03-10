6e7ccc8392b8f2c06813e55be2066f60e84134d9
h.soheili@emofid.com <h.soheili@emofid.com>
2024-03-10 08:38:52 +0330

    dd

--- main.go
+++ main.go
@@ -26,29 +26,30 @@ import (
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+	log.Print("starting server...");
+	http.HandleFunc("/", handler);
 
 	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
+	port := os.Getenv("PORT");
 	if port == "" {
-		port = "80"
-		log.Printf("defaulting to port %s", port)
+		port = "80";
+		log.Printf("defaulting to port %s", port);
 	}
 
 	// Start HTTP server.
-	log.Printf("listening on port %s", port)
+	log.Printf("listening on port %s", port);
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+		log.Fatal(err);
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	// name := os.Getenv("NAME")
+	// if name == "" {
+	// 	name = "World"
+	// }
+	// fmt.Fprintf(w, "Hello %s!\n", name)
+	w.Write([]byte("Hello"));
 }
 
 // [END run_helloworld_service]
